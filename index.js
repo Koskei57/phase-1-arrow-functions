825246b44daa248225f69b731cff28ca92bf4a4a
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-06-08 07:15:01 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+/*const foo = function(book) {
+    return 'bar';
+}
+foo('bar');*/
+const divide = (num1, num2) => 2000 / 100;
+//num1 / num2
+//divide(2000 / 100);
+const square = x => x * x;
+const add = (parameter1, parameter2) => parameter1 + parameter2;
